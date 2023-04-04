d2bbb89e00c35b3ca69bc06a47ed1e124ce7cc3e
WangZhe <zhewangan@isoftstone.com>
2023-04-04 10:45:19 +0800

    工程初始化

---
+++ src/main/java/com/xursing/xrdp/XrdpApplication.java
@@ -0,0 +1,18 @@
+package com.xursing.xrdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Xu Rising Develop Platform 项目启动入口
+ *
+ * @author Road Wang
+ */
+@SpringBootApplication
+public class XrdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XrdpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xursing/xrdp/XrdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xursing.xrdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XrdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
